f2bfc1822115fd9b041a4fe274eb061fc60c16b8
Max <maxptop@gmail.com>
2019-12-30 23:22:39 -0800

    working with the measurement selectors

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import FortyK from './images/40k.svg'
 console.log(moment().format("ddd, MMMM"))
 
 // Define Birthday
-const birthday = [1995, 5, 24]
+const birthday = "1995-06-24"
 
 const now = moment()
 // Get Birthday
@@ -22,7 +22,7 @@ const born = moment(birthday)
 const age = Math.floor(now.diff(born, 'years', true))
 const lastBday = moment(birthday).add(age, 'years')
 const nextBday = moment(birthday).add(age + 1, 'years')
-const daysToBday = nextBday.diff(now, 'days')
+const daysToBday = (nextBday.diff(now, 'days'))
 const percentBday = 100 - ((daysToBday/nextBday.diff(lastBday, 'days'))*100)
 
 // Get jan 1 date of respective years, days from 'now' to next year, and the percent of the year finished
@@ -33,11 +33,22 @@ const percentYear = 100 - ((daysToYear/nextYear.diff(thisYear, 'days'))*100)
 
 // Get number of days old, the next big date marker coming up, the date of the big day, the days until that date, and the percent of the way there
 const daysOld = now.diff(moment(birthday), 'days')
-const nextBigDay = (daysOld<10000) ? 10000 : (daysOld>10000 && daysOld<20000 ? 20000 : (daysOld>20000 && daysOld<25000 ? 25000 : (daysOld>25000 && daysOld<40000 ? 40000 : "wow, good job")))
+const nextBigDay = (daysOld<10000) 
+  ? 10000 
+  : (daysOld>10000 && daysOld<20000 
+    ? 20000 
+    : (daysOld>20000 && daysOld<25000 
+      ? 25000 
+      : (daysOld>25000 && daysOld<40000 
+        ? 40000 
+        : "wow, good job"
+        )
+      )
+  )
 const bigDayDate = moment(birthday).add(nextBigDay, 'days')
 const daysToBigDay = bigDayDate.diff(now, 'days')
 const percentDays = 100 - ((daysToBigDay/nextBigDay)*100)
-const decImage = (nextBigDay === 10000 ? TenK : (nextBigDay === 20000 ? TwentyK : (nextBday === 25000 ? Two5K : FortyK)))
+const decImage = (nextBigDay === 10000 ? TenK : (nextBigDay === 20000 ? TwentyK : (nextBigDay === 25000 ? Two5K : FortyK)))
 console.log(decImage)
 
 // Get current and next decade, date you turn the decade, days to the decade, percent of the decade
@@ -48,11 +59,11 @@ const nextDecDate = moment(birthday).add(nextDec, 'years')
 const daysToDec = nextDecDate.diff(now, 'days')
 const percentDec = 100 - ((daysToDec/nextDecDate.diff(currDecDate, 'days'))*100)
 
+console.log(moment().add(1, 'years').format("YYYY"))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="appContent">
 
       <HeroText>
         <LifeCount>{daysOld}</LifeCount>
@@ -62,44 +73,46 @@ function App() {
       <CardGrid>
 
         <Card
-          title = {`${nextBday.diff(born, 'years')}th Birthday`}
-          image = {require('./images/birthday.svg')}
-          data = {daysToBday}
-          label = "Days Remaining"
-          width = {percentBday} />
+          title={`${nextBday.diff(born, 'years')}th Birthday`}
+          image={require('./images/birthday.svg')}
+          data={daysToBday}
+          label="Days Remaining"
+          width={percentBday} 
+        />
 
         <Card
-          title = "2020 New Year"
-          image = {require('./images/newYear.svg')}
-          data = {daysToYear}
-          label = "Days Remaining"
-          width = {percentYear} />
+          title={`${moment().add(1, 'years').format("YYYY")} New Year`}
+          image={require('./images/newYear.svg')}
+          data={daysToYear}
+          label="Days Remaining"
+          width={percentYear} 
+        />
 
         <Card
-          title = {`${nextBigDay/1000}k Days`}
-          image = {decImage}
-          data = {daysToBigDay}
-          label = "Days Remaining"
-          width = {percentDays} />
+          title={`${nextBigDay/1000}k Days`}
+          image={decImage}
+          data={daysToBigDay}
+          label="Days Remaining"
+          width={percentDays} 
+        />
 
         <Card
-          title = {`${currDec}'s to ${nextDec}`}
-          image = {require('./images/death.svg')}
-          data = {daysToDec}
-          label = "Days Remaining"
-          width = {percentDec} />
-
-
+          title={`${currDec} to ${nextDec}`}
+          image={require('./images/death.svg')}
+          data={daysToDec}
+          label="Days Remaining"
+          width={percentDec}  
+        />
+    
       </CardGrid>
 
-      {/* <Measurement>
+      <Measurement>
         <span>Days Remaining</span>
         <span>Days Elapsed</span>
         <span>Percentage</span>
-      </Measurement> */}
+      </Measurement>
 
 
-      </header>
     </div>
   );
 }
@@ -111,6 +124,7 @@ const HeroText = styled.div`
   padding-top: 100px;
   animation: Rise 2.5s 0.2s forwards cubic-bezier(0.2, 0.8, 0.2, 1);
   opacity: 0;
+  text-align: center;
 `
 
 const LifeCount = styled.span`
@@ -135,10 +149,15 @@ const CardGrid = styled.div`
   transition: all ease-in-out .3s;
   opacity: 0;
   animation: Rise 2.5s 0.2s forwards cubic-bezier(0.2, 0.8, 0.2, 1);
+  justify-items: center;
+
+  div {
+    // position: absolute;
+  }
 
   @media (max-width: 720px) {
     justify-content: center;
-}
+  }
 `
 
 const Measurement = styled.div`
