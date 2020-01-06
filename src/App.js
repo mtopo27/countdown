d81d7e41fc8543d0fbfe0a2fe50203f0c7f3ddc1
Max <maxptop@gmail.com>
2020-01-06 15:40:30 -0800

    crazy shit just broke and idk what

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import Card from './components/card';
 import moment from 'moment'
 import * as util from './util'
 
-
+  /*
+    Questions:
+    1. how do I ensure something follows a format '####-##-##'
+    ---
+    4. Animating between returns? 
+  */
 
 class App extends React.Component {
   state = {
@@ -13,35 +18,13 @@ class App extends React.Component {
     hasDate: false
   }
 
-
   handleChange = (event) => {
     this.setState({dateInput: event.target.value});
   }
 
-  // handleChange = (event) => {
-  //   this.setState((state) => {
-  //     return {dateInput: event.target.value}
-  //   })
-  // }
-
   handleClick = (event) => {
     this.setState({hasDate: true});
   }
-
-  handleClick = (event) => {
-    this.setState((state) => {
-      return {hasDate: true};
-    })
-  }
-
-  /*
-    Questions:
-    1. If nothing outside of this class can see the value of {this.state.dateInput}, how can I use the inputted value in my util file or do I need to put all my date logic that takes the input value inside this class
-    2. how do I ensure something follows a format '####-##-##'
-    3. Any idea why the styled components don't render after state change
-    ---
-    4. Animating between returns? 
-  */
 
   render() {
     // Constants from states, logic functions held in util
@@ -53,28 +36,74 @@ class App extends React.Component {
     const [nextBigDay, decImage, daysToBigDay, percentDays] = util.daysOldData(userBday)
     const [currDec, nextDec, daysToDec, percentDec] = util.decData(userAges, userBday)
 
-    if (!this.state.hasDate) {
-      return (
-        <div className="appContent">
-        <TestInput>
-        <input 
-          type="text" 
-          onChange={this.handleChange} >
-        </input>
-        <button onClick={this.handleClick}>Store Date</button>
-        {this.state.dateInput === "hello" ? <span>yo</span> : <span>nope</span>}
-        </TestInput>
-        </div>
-      )
-    }
-
-    return (
-      <div className="appContent">
-        <HeroText>
-          <LifeCount>{userDaysOld}</LifeCount>
-          <LifeLabel>Days Old</LifeLabel>
-        </HeroText>
+    const TestInput = styled.div`
+    position: absolute; 
+    left: 40%;
+    top: 40%;
   
+    input {
+      border: solid 1px black;
+    }
+  
+    button {
+      background-color: rgba(0, 0, 0, .1);
+      color: white;
+    }
+  `
+  
+  const HeroText = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 60px;
+    padding-top: 100px;
+    animation: Rise 2.5s 0.2s forwards cubic-bezier(0.2, 0.8, 0.2, 1);
+    opacity: 0;
+  `
+  
+  const LifeCount = styled.span`
+    font-size: 3rem;
+    color: white;
+    font-weight: bold;
+  `
+  
+  const LifeLabel = styled.span`
+    font-size: 2rem;
+    color: rgba(255, 255, 255, .8);
+  `
+  
+  const CardGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat( auto-fit, 290px );
+    grid-gap: 60px 40px;
+    padding-left: 50px;
+    padding-right: 50px;
+    justify-content: space-between;
+    transition: all ease-in-out .3s;
+    position-relative
+    opacity: 0;
+    animation: Rise 2.5s 0.2s forwards cubic-bezier(0.2, 0.8, 0.2, 1);
+  
+    @media (max-width: 720px) {
+      justify-content: center;
+  }
+  `
+
+    const pageInput =(
+    <TestInput>
+      <input 
+        type="text" 
+        onChange={this.handleChange}>
+      </input>
+      <button onClick={this.handleClick}>Store Date</button>
+    </TestInput>)
+
+    const dataCards =(
+      <>
+      <HeroText>
+        <LifeCount>{userDaysOld}</LifeCount>
+        <LifeLabel>Days Old</LifeLabel>
+      </HeroText>
+    
         <CardGrid>
           <Card
             title={`${userAges + 1}th Birthday`}
@@ -103,12 +132,74 @@ class App extends React.Component {
             data={daysToDec}
             label="Days Remaining"
             width={percentDec} />
-        </CardGrid>
-      </div>
-  );
-    }
-    }
+        </CardGrid></>)
+    
+      return (
+        <div className="appContent">
+          {!this.state.hasDate ? 
+            (<TestInput>
+              <input 
+                type="text" 
+                onChange={this.handleChange}>
+              </input>
+              <button onClick={this.handleClick}>Store Date</button>
+            </TestInput>) : ( <>
+      <HeroText>
+        <LifeCount>{userDaysOld}</LifeCount>
+        <LifeLabel>Days Old</LifeLabel>
+      </HeroText>
+    
+        <CardGrid>
+          <Card
+            title={`${userAges + 1}th Birthday`}
+            image={require('./images/birthday.svg')}
+            data={daysToBday}
+            label="Days Remaining"
+            width={percentBday} />
 
+          <Card
+            title={`${moment().add(1, 'years').format("YYYY")} New Year`}
+            image={require('./images/newYear.svg')}
+            data={util.daysToYear}
+            label="Days Remaining"
+            width={util.percentYear} />
+  
+          <Card
+            title={`${nextBigDay/1000}k Days`}
+            image={decImage}
+            data={daysToBigDay}
+            label="Days Remaining"
+            width={percentDays} />
+  
+          <Card
+            title={`${currDec}'s to ${nextDec}`}
+            image={require('./images/death.svg')}
+            data={daysToDec}
+            label="Days Remaining"
+            width={percentDec} />
+        </CardGrid></>)
+
+        }
+        </div>
+     );
+  }
+}
+
+
+const TestInput = styled.div`
+  position: absolute; 
+  left: 40%;
+  top: 40%;
+
+  input {
+    border: solid 1px black;
+  }
+
+  button {
+    background-color: rgba(0, 0, 0, .1);
+    color: white;
+  }
+`
 
 const HeroText = styled.div`
   display: flex;
@@ -145,21 +236,6 @@ const CardGrid = styled.div`
   @media (max-width: 720px) {
     justify-content: center;
 }
-`
-
-const TestInput = styled.div`
-  position: absolute; 
-  left: 40%;
-  top: 40%;
-
-  input {
-    border: solid 1px black;
-  }
-
-  button {
-    background-color: rgba(0, 0, 0, .1);
-    color: white;
-  }
 `
 
 export default App;
